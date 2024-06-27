c6f1637187763fca8337bf459dc8548448ecf8f7
jrbarazone <jrlezama@barazone.com.mx>
2024-06-27 10:12:26 -0600

    Update data_storage.py

--- scripts/data_storage.py
+++ scripts/data_storage.py
@@ -1,12 +1,7 @@
-import gspread
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Configuración de Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_json_keyfile', scope)
-client = gspread.authorize(creds)
-
-# Función para almacenar datos en Google Sheets
-def save_to_sheet(barcode, image_url, quantity, description, title):
-    sheet = client.open("Product Data").sheet1
-    sheet.append_row([barcode, image_url, quantity, description, title])
+keyfile_path = '/app/sqlbarazone-2fdbb67f3964.json'
+creds = ServiceAccountCredentials.from_json_keyfile_name(keyfile_path, scope)
